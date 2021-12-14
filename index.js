8b60fb7c959237c7175fe4b8ba9f36ac788e19bd
dziaduer <dziaduer@gmail.com>
2021-12-14 18:57:01 +0100

    Added calculation order, precentages, powers, coma

--- index.js
+++ index.js
@@ -5,10 +5,56 @@ function Add(num1, num2) {
     return num1 + num2;
 }
 function Calculate(values, tokens) {
-    var result = +values[0];
     var i = 0;
     for (var _i = 0, tokens_1 = tokens; _i < tokens_1.length; _i++) {
         var token = tokens_1[_i];
+        switch (token) {
+            case '%':
+                values[i] = (+values[i] / 100);
+                tokens.splice(i, 1);
+                values.splice(i + 1, 1);
+                break;
+            case ',':
+                values[i] = +(values[i] + '.' + values[i + 1]);
+                tokens.splice(i, 1);
+                values.splice(i + 1, 1);
+                break;
+        }
+        i++;
+    }
+    for (var i_1 = 0; i_1 < tokens.length;) {
+        switch (tokens[i_1]) {
+            case '*':
+                values[i_1] = (+values[i_1] * +values[i_1 + 1]);
+                tokens.splice(i_1, 1);
+                values.splice(i_1 + 1, 1);
+                break;
+            case '/':
+                if (+values[i_1 + 1] != 0) {
+                    values[i_1] = +values[i_1] / +values[i_1 + 1];
+                    tokens.splice(i_1, 1);
+                    values.splice(i_1 + 1, 1);
+                }
+                else {
+                    values[i_1] = +values[i_1] / 1;
+                    tokens.splice(i_1, 1);
+                    values.splice(i_1 + 1, 1);
+                }
+                break;
+            case '^':
+                values[i_1] = Math.pow(+values[i_1], +values[i_1 + 1]);
+                tokens.splice(i_1, 1);
+                values.splice(i_1 + 1, 1);
+                break;
+            default:
+                i_1++;
+                break;
+        }
+    }
+    var result = +values[0];
+    i = 0;
+    for (var _a = 0, tokens_2 = tokens; _a < tokens_2.length; _a++) {
+        var token = tokens_2[_a];
         i++;
         switch (token) {
             case '+':
@@ -16,12 +62,6 @@ function Calculate(values, tokens) {
                 break;
             case '-':
                 result = result - +values[i];
-                break;
-            case '*':
-                result = result * +values[i];
-                break;
-            case '/':
-                result = result / +values[i];
                 break;
         }
     }
@@ -46,6 +86,15 @@ function GetCalculationTokens(text) {
             case '/':
                 tokens.push('/');
                 break;
+            case '%':
+                tokens.push('%');
+                break;
+            case ',':
+                tokens.push(',');
+                break;
+            case '^':
+                tokens.push('^');
+                break;
             default:
                 break;
         }
@@ -53,7 +102,7 @@ function GetCalculationTokens(text) {
     return tokens;
 }
 function ParseToArray(text) {
-    return text.split(/[\+\-\*\/]/);
+    return text.split(/[\+\-\*\/\%\,\^]/);
 }
 function SubmitCalculation() {
     answerHeader.innerText = Calculate(ParseToArray(input.value), GetCalculationTokens(input.value));
--- index.ts
+++ index.ts
@@ -6,9 +6,56 @@ function Add(num1: number, num2: number){
     return num1 + num2;
 }
 
-function Calculate(values: Array<string>, tokens: Array<operator>): string{
-    var result = +values[0];
+function Calculate(values, tokens: Array<operator>): string{
     let i = 0;
+    for(let token of tokens){
+        switch(token){
+            case '%':
+                values[i] = (+values[i] / 100);
+                tokens.splice(i, 1);
+                values.splice(i + 1, 1);
+                break;
+            case ',':
+                    values[i] = +(values[i] + '.' + values[i + 1]);
+                    tokens.splice(i, 1);
+                    values.splice(i + 1, 1);
+                    break;
+        }
+        i++;
+    }
+    for(let i = 0; i < tokens.length;){
+        switch(tokens[i]){
+            case '*':
+                values[i] = (+values[i] * +values[i+1]);
+                tokens.splice(i, 1);
+                values.splice(i + 1, 1);
+                break;
+            case '/':
+                if(+values[i + 1] != 0)
+                {
+                    values[i] = +values[i] / +values[i+1];
+                    tokens.splice(i, 1);
+                    values.splice(i + 1, 1);
+                }
+                else{
+                    values[i] = +values[i] / 1;
+                    tokens.splice(i, 1);
+                    values.splice(i + 1, 1);
+                }
+                break;
+            case '^':
+                    values[i] = Math.pow(+values[i], +values[i+1]); 
+                    tokens.splice(i, 1);
+                    values.splice(i + 1, 1);
+                break;
+            default:
+                i++;
+                break;
+        }
+        
+    }
+    var result = +values[0];
+    i = 0;
     for(let token of tokens){
         i++;
         switch(token){
@@ -18,18 +65,12 @@ function Calculate(values: Array<string>, tokens: Array<operator>): string{
             case '-':
                 result = result - +values[i];
                 break;
-            case '*':
-                result = result * +values[i];
-                break;
-            case '/':
-                result = result / +values[i];
-                break;
         }
     }
     return result.toString();
 }
 
-type operator = '+' | '-' | '*' | '/';
+type operator = '+' | '-' | '*' | '/' | '%' | ',' | '^';
 
 function GetCalculationTokens(text: string){
     let tokens: Array<operator>;
@@ -49,6 +90,15 @@ function GetCalculationTokens(text: string){
             case '/':
                 tokens.push('/');
                 break;
+            case '%':
+                tokens.push('%');
+                break;
+            case ',':
+                    tokens.push(',');
+                    break;
+            case '^':
+                tokens.push('^');
+                break;
             default:
                 break;
         }
@@ -57,7 +107,7 @@ function GetCalculationTokens(text: string){
 }
 
 function ParseToArray(text: string){
-return text.split(/[\+\-\*\/]/);
+return text.split(/[\+\-\*\/\%\,\^]/);
 }
 
 function SubmitCalculation()
